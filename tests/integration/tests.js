ab943f8b6b8d9f0697f3d05f0187462703bb5ca7
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-22 17:53:24 +0000

    Unit tests complete

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,4 +16,14 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should be able to add two numbers together', function(){
+    //we want to add 1 and 4 = 5
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  })
+
 });
--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -11,4 +11,75 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
-});
+  it('it can add 1 and 4 to give 5', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.strictEqual(calculator.runningTotal, 5);
+  })
+
+  it('it can subtract 4 from 7 to give 3', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.strictEqual(calculator.runningTotal, 3);
+  })
+
+  it('it can multiply 3 by 5 to give 15', function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.strictEqual(calculator.runningTotal, 15);
+  })
+
+  it('it can divide 21 by 7 to get 3', function(){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.strictEqual(calculator.runningTotal, 3);
+  })
+
+  it('it can concatenate multiple number button clicks', function(){
+    calculator.numberClick(1);
+    calculator.numberClick(5);
+    calculator.numberClick(9);
+    assert.strictEqual(calculator.runningTotal, 159);
+  })
+
+  it('it can chain multiple operations together', function(){
+    calculator.previousTotal = 0;
+
+    calculator.numberClick(9);      // 9
+    calculator.operatorClick('+');
+    calculator.numberClick(9);      // 18
+    calculator.operatorClick('-');
+    calculator.numberClick(15);     // 3
+    calculator.operatorClick('*');
+    calculator.numberClick(4);      // 12
+    calculator.operatorClick('/');
+    calculator.numberClick(2);      // 6
+    calculator.operatorClick('=');
+     
+    assert.strictEqual(calculator.runningTotal, 6);
+
+  })
+
+
+  it('clear the running total without affecting the calculation', function(){
+    calculator.previousTotal = 0;
+
+    calculator.numberClick(9);      // 9
+    calculator.operatorClick('+');
+    calculator.numberClick(9);      // 18
+    calculator.operatorClick('-');
+    calculator.numberClick(333);    // That is not the number we wanted!!!
+    calculator.clearClick()
+    calculator.numberClick(15);     // 3
+    calculator.operatorClick('*');
+    calculator.numberClick(4);      // 12
+    calculator.operatorClick('/');
+    calculator.numberClick(2);      // 6
+    calculator.operatorClick('=');
+     
+    assert.strictEqual(calculator.runningTotal, 6);
+
+  })
+
+
+})
